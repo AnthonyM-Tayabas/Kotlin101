ce9116d07a16f2021024b0d79232181fa36d20cc
Anthony Tayabas <anthonymark.tayabas@cognizant.com>
2023-06-30 05:05:07 +0800

    Added Sample Code about Activity Lifecycle

--- app/src/main/java/csv/masters/kotlin101/main/MainActivity.kt
+++ app/src/main/java/csv/masters/kotlin101/main/MainActivity.kt
@@ -4,12 +4,14 @@ import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import csv.masters.kotlin101.databinding.ActivityMainBinding
 import csv.masters.kotlin101.second.SecondActivity
 import csv.masters.kotlin101.second.SecondActivity.Companion.EXTRA_REPLY
+import csv.masters.kotlin101.third.ThirdActivity
 
 // A: Implementing Click Listener via Inheritance
 //class MainActivity : AppCompatActivity(), View.OnClickListener {
@@ -48,9 +50,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        Log.d("Lifecycle", "${MainActivity::class.java.simpleName} onCreate()")
+
         savedInstanceState?.let { savedBundle ->
             binding.tvMyText.text = savedBundle.getString(STATE_MESSAGE, "Hello World")
         }
+
+        Log.d(MainActivity::class.java.simpleName, "This is debug")
+        Log.e(MainActivity::class.java.simpleName, "This is error")
+        Log.w(MainActivity::class.java.simpleName, "This is warning")
+        Log.i(MainActivity::class.java.simpleName, "This is info")
+        Log.v(MainActivity::class.java.simpleName, "This is verbose")
 
         with(binding) {
             btClick.setOnClickListener {
@@ -69,11 +79,18 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
             }
 
+            btOpenThird?.setOnClickListener {
+                startActivity(Intent(this@MainActivity, ThirdActivity::class.java))
+            }
+
             // Opening a new activity expecting a result from second page
             btOpenActivityForResults?.setOnClickListener {
                 val intent = Intent(this@MainActivity, SecondActivity::class.java)
                 intent.putExtra(SecondActivity.EXTRA_MESSAGE, "First page needs your reply")
                 startForResult.launch(intent)
+                val intentForThirdActivity = Intent(this@MainActivity, ThirdActivity::class.java)
+                startForResultOnPage3.launch(intentForThirdActivity)
+                //startActivityForResult()
             }
 
             // Implicit intent to open URL via device's default browser app
@@ -94,12 +111,45 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d("Lifecycle", "${MainActivity::class.java.simpleName} onStart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Lifecycle", "${MainActivity::class.java.simpleName} onResume()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Lifecycle", "${MainActivity::class.java.simpleName} onPause()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("Lifecycle", "${MainActivity::class.java.simpleName} onStop()")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("Lifecycle", "${MainActivity::class.java.simpleName} onRestart()")
+    }
+
     // A: Implementing Click Listener via Inheritance
 //    override fun onClick(p0: View?) {
 //        textView.text = "Kotlin Masters 101 From Inheritance"
 //    }
 
     private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == Activity.RESULT_OK) {
+            it.data?.let { intent ->
+                binding.tvResponse?.text = intent.getStringExtra(EXTRA_REPLY)
+            }
+        }
+    }
+
+    private val startForResultOnPage3 = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
         if (it.resultCode == Activity.RESULT_OK) {
             it.data?.let { intent ->
                 binding.tvResponse?.text = intent.getStringExtra(EXTRA_REPLY)
--- app/src/main/java/csv/masters/kotlin101/third/ThirdActivity.kt
+++ app/src/main/java/csv/masters/kotlin101/third/ThirdActivity.kt
@@ -1,16 +1,44 @@
 package csv.masters.kotlin101.third
 
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import csv.masters.kotlin101.R
+import csv.masters.kotlin101.databinding.ActivityThirdBinding
 
 class ThirdActivity: AppCompatActivity() {
 
+    private lateinit var binding: ActivityThirdBinding
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-        setContentView(R.layout.activity_third)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityThirdBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Log.d("Lifecycle", "${ThirdActivity::class.java.simpleName} onCreate()")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Lifecycle", "${ThirdActivity::class.java.simpleName} onStart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Lifecycle", "${ThirdActivity::class.java.simpleName} onResume()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Lifecycle", "${ThirdActivity::class.java.simpleName} onPause()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("Lifecycle", "${ThirdActivity::class.java.simpleName} onStop()")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Lifecycle", "${ThirdActivity::class.java.simpleName} onDestroy()")
     }
 
 
