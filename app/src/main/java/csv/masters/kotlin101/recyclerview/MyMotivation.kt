8ba23ac9140cbcbfe1010091f460868825121f6c
Anthony Tayabas <anthonymark.tayabas@cognizant.com>
2023-07-11 10:26:39 +0800

    Initial commit for recycler view

---
+++ app/src/main/java/csv/masters/kotlin101/recyclerview/MyMotivation.kt
@@ -0,0 +1,12 @@
+package csv.masters.kotlin101.recyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import csv.masters.kotlin101.R
+
+class MyMotivation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_motivation)
+    }
+}
